f66a181b85b50c67c2bf2b86ad574f3f93058bcd
dsolguin <dsolguin@ucsb.edu>
2023-10-13 17:29:03 -0700

    17:28

--- assignment_2.R
+++ assignment_2.R
@@ -1,4 +1,34 @@
-## tells Git to track all changes in this RStudio Project
-usethis::use_git()
-## automatically syncs the Rstudio Project with your Github account
-usethis::use_github()
+# Sofia Olguin
+rm(list=ls()) # clear the environment
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+#-------Import necessary packages here-------------------#
+# Note: Using other packages than the ones listed below can break the autograder
+library(tidyverse) # importing a package
+
+#------ Uploading PERMID --------------------------------#
+PERMID <- "8277717" #Type your PERMID within the quotation marks
+PERMID <- as.numeric(gsub("\\D", "", PERMID)) #Don't touch
+set.seed(PERMID) #Don't touch
+
+#------ Answers -----------------------------------------#
+
+# Part 2: Coding
+
+## 1. Setting up the data.
+
+#------ Uploading Data ----------------------------------#
+# Note: - DO NOT use read.csv()
+# - DO NOT include filepath inside the quotations, just the .csv file name
+# - DO NOT rename the dataset (the .csv file)
+airbnb <- read_csv("airbnb_data.csv") 
+
+airbnb <- airbnb %>% rename( , neighborhood = neighbourhood)
+
+
+## 2. Piping practice and creating summary statistics.
+
+neighborhoods <- airbnb %>%
+  filter()
+
+
