764ee34186930133e37f9b1e96ac7c91ef44930a
Brooksby-Austin <brooksby.austin@gmail.com>
2023-10-13 17:26:32 -0700

    NaN still appearing in summary stats

--- assignment_2.R
+++ assignment_2.R
@@ -10,11 +10,6 @@ PERMID <- "7216567" #Type your PERMID within the quotation marks
 PERMID <- as.numeric(gsub("\\D", "", PERMID)) #Don't touch
 set.seed(PERMID) #Don't touch
 
-## tells Git to track all changes in this RStudio Project
-usethis::use_git()
-## automatically syncs the Rstudio Project with your Github account
-usethis::use_github()
-
 #1
 airbnb <- read.csv("airbnb_data.csv")
 airbnb <- airbnb %>% rename("neighborhood" = "neighbourhood")
