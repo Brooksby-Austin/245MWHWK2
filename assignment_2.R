797fdd04095dceaff44d84fa78377364a4c23466
dsolguin <dsolguin@ucsb.edu>
2023-10-13 18:59:34 -0700

    18:59

--- assignment_2.R
+++ assignment_2.R
@@ -23,12 +23,27 @@ set.seed(PERMID) #Don't touch
 # - DO NOT rename the dataset (the .csv file)
 airbnb <- read_csv("airbnb_data.csv") 
 
-airbnb <- airbnb %>% rename( , neighborhood = neighbourhood)
+airbnb <- airbnb %>% rename(neighborhood = neighbourhood)
 
 
 ## 2. Piping practice and creating summary statistics.
 
-neighborhoods <- airbnb %>%
-  filter()
+neighborhoods <- airbnb %>% select(neighborhood) %>%
+  filter(!is.na(neighborhood)) %>%  count(neighborhood) %>%
+  arrange(desc(n)) %>% 
+  head(20)
 
+airbnb_top_neighborhoods <- airbnb %>% filter(airbnb$neighborhood %in% neighborhoods$neighborhood)
+
+summary_stats_top_neighborhoods <- airbnb_top_neighborhoods %>% group_by(
+  neighborhood) %>% 
+  summarize(avg_square_feet = mean(square_feet, na.rm=TRUE),
+                              avg_price = mean(price, na.rm=TRUE), 
+            sd_price = sd(price, na.rm=TRUE),
+            max_price = max(price, na.rm=TRUE), 
+            min_price = min(price, na.rm=TRUE) ) %>% arrange(desc(avg_square_feet))
+  
+highest_avg_square_ft <- summary_stats_top_neighborhoods %>% select(avg_square_feet) %>% slice(1) %>% pull(avg_square_feet)
+
+second_avg_price <- summary_stats_top_neighborhoods %>% select(avg_price) %>% slice(2) %>% pull(avg_price)
 
